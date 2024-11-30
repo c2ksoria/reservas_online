4d70e8657559e8cb7c73ad66aa4b8898272c2e18
Carlos Valdemar Soria <carlosvsoria1982@gmail.com>
2024-11-30 12:18:48 -0300

    Limpieza de código para mantener el orden del código

--- reservas/app/urls.py
+++ reservas/app/urls.py
@@ -1,8 +1,5 @@
-
-from django.urls import path, include
-
+from django.urls import path
 from .views import index, ReservationList, ReservaCreateView, Formulario_nueva_reserva, Formulario_update_reserva, Formulario_nuevo_pago
-
 
 urlpatterns = [
     path('', index, name='index'),
@@ -10,6 +7,5 @@ urlpatterns = [
     path('reservations/add', ReservaCreateView.as_view(), name='nwereserva'),
     path('reservations/form', Formulario_nueva_reserva, name='form_new_reserva'),
     path('reservations/update/<int:pk>', Formulario_update_reserva, name='form_update_reserva'),
-
     path('payments/form', Formulario_nuevo_pago, name='form_update_reserva'),
 ]
